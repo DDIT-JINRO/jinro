149ee64160036eab6c6727bb1388cbb58a143e71
NZM <nzm6846@gmail.com>
2025-07-18 12:49:24 +0900

    feat : errorEnum 코드 구현해둠

---
+++ src/main/java/kr/or/ddit/util/exception/ErrorCode.java
@@ -0,0 +1,16 @@
+package kr.or.ddit.util.exception;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+		INVALID_INPUT("E400", "잘못된 요청입니다.");
+
+	
+	  private final String code;
+	  private final String description;
+}
