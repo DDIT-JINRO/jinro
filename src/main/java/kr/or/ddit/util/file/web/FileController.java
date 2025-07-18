6bbc286bdb4d6d775dbc93f4f6ba9be474b4f734
NZM <nzm6846@gmail.com>
2025-07-18 15:24:31 +0900

    feat : 파일 다운로드 컨트롩러 추가

---
+++ src/main/java/kr/or/ddit/util/file/web/FileController.java
@@ -0,0 +1,45 @@
+package kr.or.ddit.util.file.web;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.List;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kr.or.ddit.util.file.service.FileDetailVO;
+import kr.or.ddit.util.file.service.FileService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/files")
+public class FileController {
+	
+	private final FileService fileService;
+	  
+    @GetMapping("/download")
+    public ResponseEntity<Resource> download(
+            @RequestParam String groupId,
+            @RequestParam int seq
+    ) throws IOException {
+        FileDetailVO detail = fileService.getFileDetail(groupId, seq);
+        Resource resource = fileService.downloadFile(groupId, seq);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        "attachment; filename=\"" + URLEncoder.encode(detail.getFileOrgName(), "UTF-8") + "\"")
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .body(resource);
+    }
+
+
+}
