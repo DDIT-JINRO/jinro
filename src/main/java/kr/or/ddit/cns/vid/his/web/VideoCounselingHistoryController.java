3a91d79b191fce416624a8534006c175d15956d6
Lee Seung Ho <leeseungho55@icloud.com>
2025-07-21 15:28:32 +0900

    feat: 패키지.web 수정

--- src/main/java/kr/or/ddit/cns/vid/his/web/VideoCounselingHistoryController.java
+++ src/main/java/kr/or/ddit/cns/vid/his/web/VideoCounselingHistoryController.java
@@ -1,4 +1,4 @@
-package kr.or.ddit.cns.vid.his;
+package kr.or.ddit.cns.vid.his.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
