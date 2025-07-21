e0fa09806ce2a3084f2b08c65fb211131fd8732c
NZM <nzm6846@gmail.com>
2025-07-21 17:07:04 +0900

    fix : WebConfig 정적자원 예외 등록

---
+++ src/main/java/kr/or/ddit/config/WebConfig.java
@@ -0,0 +1,24 @@
+package kr.or.ddit.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // classpath:/static/css/ → /css/**
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css/");
+
+        // classpath:/static/images/ → /images/**
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/static/images/");
+        
+     // classpath:/static/fonts/ → /fonts/**
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts/");
+    }
+}
+
