c348cd1a8df39ebd2711b9505b817dac30e46e10
nzm <nzm6846@gmail.com>
2025-07-22 22:59:26 +0900

    feat : 자기소개서 서비스, 매퍼, VO 생성

---
+++ src/main/java/kr/or/ddit/cdp/sint/service/impl/SelfIntroServiceImpl.java
@@ -0,0 +1,10 @@
+package kr.or.ddit.cdp.sint.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.cdp.sint.service.SelfIntroService;
+
+@Service
+public class SelfIntroServiceImpl implements SelfIntroService {
+
+}
