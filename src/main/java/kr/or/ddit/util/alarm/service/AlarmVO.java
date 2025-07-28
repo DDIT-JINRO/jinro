85e89e4487b1ff8185132f6913ddccb184f1599f
5e0k1 <ksc03073@gmail.com>
2025-07-28 12:39:09 +0900

    refactor : 변경된 테이블컬럼명에 맞게 VO 수정 및 설명 첨부

--- src/main/java/kr/or/ddit/util/alarm/service/AlarmVO.java
+++ src/main/java/kr/or/ddit/util/alarm/service/AlarmVO.java
@@ -6,12 +6,14 @@ import lombok.Data;
 
 @Data
 public class AlarmVO {
-	private int alarmId;
-	private int memId;
-	private String alarmTargetType;
-	private String alarmContent;
-	private String alarmIsRead;
-	private Date alarmCreatedAt;
-	private int boardId;
-	private String replyId;
+	private int alarmId;			// 알림번호
+	private int memId;				// 회원번호(알림받을 대상)
+	private AlarmType alarmTargetType;	// 알림유형(공통코드)
+	private String alarmContent;	// 알림메시지,내용
+	private String alarmIsRead;		// 알림읽음 여부. 삭제처리는 delete
+	private Date alarmCreatedAt;	// 알림생성일시
+	private int alarmTargetId;		// 알림대상번호 (좋아요나 댓글이 달린 대상의 번호)
+	private String alarmTargetUrl;	// 알림대상url (알림클릭시 이동시킬 url)
+	
 }
+
