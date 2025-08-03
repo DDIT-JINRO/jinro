1c32fe1b8e2490962c7b62fd1fe81b15f202898f
KangBoram <kb6408@naver.com>
2025-08-03 20:37:28 +0900

    refactor : 자소서 로직 변경

--- src/main/java/kr/or/ddit/mpg/mat/sih/web/SelfIntroHistoryController.java
+++ src/main/java/kr/or/ddit/mpg/mat/sih/web/SelfIntroHistoryController.java
@@ -1,5 +1,7 @@
 package kr.or.ddit.mpg.mat.sih.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import kr.or.ddit.cdp.sint.service.SelfIntroService;
 import kr.or.ddit.cdp.sint.service.SelfIntroVO;
+import kr.or.ddit.mpg.mat.reh.web.ResumeVO;
 import kr.or.ddit.mpg.mat.sih.service.SelfIntroHistoryService;
 import kr.or.ddit.util.ArticlePage;
 import lombok.extern.slf4j.Slf4j;
@@ -17,24 +21,40 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/mpg")
 @Slf4j
 public class SelfIntroHistoryController {
-
+	
 	@Autowired
-	SelfIntroHistoryService selfIntroHistoryService;
+	SelfIntroService selfIntroService;
 	
 	@GetMapping("/mat/sih/selectSelfIntroHistoryList.do")
-	public String selectSelfIntroHistoryList (@AuthenticationPrincipal String memId
+	public String selectSelfIntroHistoryList(@AuthenticationPrincipal String memId
 			, @RequestParam(required = false) String keyword
 			, @RequestParam(required = false) String status
 			, @RequestParam(required = false, defaultValue = "1") int currentPage
 			, @RequestParam(required = false, defaultValue = "5") int size
 			, Model model) {
 		
-		ArticlePage<SelfIntroVO> articlePage = this.selfIntroHistoryService.selectSelfIntroHistoryList(memId, keyword, status, currentPage, size);
+		if (memId != null && !"anonymousUser".equals(memId)) {
+			SelfIntroVO selfIntroVO = new SelfIntroVO();
+			selfIntroVO.setMemId(Integer.parseInt(memId));
+			selfIntroVO.setKeyword(keyword);
+			selfIntroVO.setStatus(status);
+			selfIntroVO.setCurrentPage(currentPage);
+			selfIntroVO.setSize(size);
+			
+			int total = this.selfIntroService.selectSelfIntroTotalBymemId(selfIntroVO);
+			List<SelfIntroVO> selfIntroVOList = this.selfIntroService.selectSelfIntroBymemId(selfIntroVO);
+			
+			ArticlePage<SelfIntroVO> articlePage = new ArticlePage<SelfIntroVO>(total, currentPage, size, selfIntroVOList, keyword);
+			
+			articlePage.setUrl("/mpg/mat/sih/selectSelfIntroHistoryList.do");
 
-		if(articlePage != null) {
-			model.addAttribute("articlePage", articlePage);
+			if(articlePage != null) {
+				model.addAttribute("articlePage", articlePage);
+			}
+
+			return "mpg/mat/sih/selectSelfIntroHistoryList";
+		} else {
+			return "redirect:/login";
 		}
-		
-		return "mpg/mat/sih/selectSelfIntroHistoryList";
 	}
 }
