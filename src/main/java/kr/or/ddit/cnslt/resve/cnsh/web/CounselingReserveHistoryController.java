99caf69e816d6768d58022585ed1ae6c420798c1
NZM <nzm6846@gmail.com>
2025-08-09 14:21:55 +0900

    feat: url // 로그인 redirect붙여서 수정

--- src/main/java/kr/or/ddit/cnslt/resve/cnsh/web/CounselingReserveHistoryController.java
+++ src/main/java/kr/or/ddit/cnslt/resve/cnsh/web/CounselingReserveHistoryController.java
@@ -1,14 +1,18 @@
 package kr.or.ddit.cnslt.resve.cnsh.web;
 
 import java.security.Principal;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.ddit.cnslt.resve.crsv.service.CounselingReserveService;
 import kr.or.ddit.cnslt.resve.crsv.service.CounselingVO;
+import kr.or.ddit.mpg.mat.csh.service.CounselingHistoryService;
+import kr.or.ddit.util.ArticlePage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,20 +22,33 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class CounselingReserveHistoryController {
 	
-	CounselingReserveService counselingReserveService;
+	CounselingHistoryService counselingHistoryService;
 	
 	@GetMapping("/cnsh/counselingReserveHistory.do")
 	public String counselingHistory(
 			@ModelAttribute CounselingVO counselingVO,
-			Principal principal) {
+			Principal principal,
+			Model model) {
 		
 	    if(principal!=null && !principal.getName().equals("anonymousUser")) {
-	         
+	    	
+	    	String memId = principal.getName();
+	    	
+			Map<String, String> counselStatus = counselingHistoryService.selectCounselStatusList();
+			Map<String, String> counselCategory = counselingHistoryService.selectCounselCategoryList();
+			Map<String, String> counselMethod = counselingHistoryService.selectCounselMethodList();
+			ArticlePage<CounselingVO> articlePage = counselingHistoryService.selectCounselingList(memId, counselingVO);
+			articlePage.setUrl("/cnslt/resve/cnsh/counselingReserveHistory.do");
+			
+			model.addAttribute("counselStatus", counselStatus);
+			model.addAttribute("counselCategory", counselCategory);
+			model.addAttribute("counselMethod", counselMethod);
+			model.addAttribute("articlePage", articlePage);
+			
+			return "cnslt/resve/cnsh/counselingreservehistory";
 	      }else {
-	    	  return "/login";
+	    	  return "redirect:/login";
 	      }
-	    
-		return "cnslt/resve/cnsh/counselingreservehistory";
 	}
 	
 }
