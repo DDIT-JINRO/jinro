e79932590899d0ea943249863d719240696e94c4
KangBoram <kb6408@naver.com>
2025-08-11 14:20:08 +0900

    refactor : 기업당 리뷰 2개씩 출력하는 로직으로 변경

--- src/main/java/kr/or/ddit/empt/enp/service/impl/EnterprisePostingServiceImpl.java
+++ src/main/java/kr/or/ddit/empt/enp/service/impl/EnterprisePostingServiceImpl.java
@@ -10,10 +10,8 @@ import kr.or.ddit.empt.enp.service.EnterprisePostingService;
 import kr.or.ddit.empt.enp.service.InterviewReviewVO;
 import kr.or.ddit.mpg.mat.bmk.service.BookMarkVO;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class EnterprisePostingServiceImpl implements EnterprisePostingService {
 
