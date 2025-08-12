7df6a01ceb639024a3cf8a4552887fc0fd661a38
Lee Seung Ho <leeseungho55@icloud.com>
2025-08-12 15:15:55 +0900

    feat: 직업체험 CRUD

--- src/main/java/kr/or/ddit/empt/cte/web/CareerTechnicalEducationController.java
+++ src/main/java/kr/or/ddit/empt/cte/web/CareerTechnicalEducationController.java
@@ -1,16 +1,34 @@
 package kr.or.ddit.empt.cte.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.or.ddit.empt.cte.service.CareerTechnicalEducationService;
+import kr.or.ddit.empt.cte.service.CareerTechnicalEducationVO;
+import kr.or.ddit.util.ArticlePage;
 
 @Controller
 @RequestMapping("/empt")
 public class CareerTechnicalEducationController {
 	
+	@Autowired
+	private CareerTechnicalEducationService educationService;
+	
 	@GetMapping("/cte/careerTechnicalEducation.do")
-	public String careerTechEduList() {
+	public String careerTechEduList( @RequestParam(required = false) String keyword,
+		    @RequestParam(required = false) String region,
+		    @RequestParam(required = false) String status,
+		    @RequestParam(value="currentPage",required=false, defaultValue="1") int currentPage,
+		    @RequestParam(value="size",required=false, defaultValue = "10")int size, Model model) {
 		
+			ArticlePage<CareerTechnicalEducationVO> articlePage = educationService.getList(keyword,region,status,currentPage,size);
+			articlePage.setUrl("/empt/cte/careerTechnicalEducation.do");
+			model.addAttribute("articlePage", articlePage);
+			
 		return "empt/cte/careerTechnicalEducation";
 	}
 	
