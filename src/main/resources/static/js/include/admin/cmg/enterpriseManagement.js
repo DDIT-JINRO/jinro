7a2aa1d50208baaa3797252a388e444cfd2b4cc6
jootaehyeon <jooth5501@gmail.com>
2025-08-12 23:46:04 +0900

    feat : 기업관리 js 수정

--- src/main/resources/static/js/include/admin/cmg/enterpriseManagement.js
+++ src/main/resources/static/js/include/admin/cmg/enterpriseManagement.js
@@ -18,9 +18,9 @@ function fetchEntList(page = 1) {
 		}
 	})
 		.then(({ data }) => {
-			
+
 			console.log(data);
-			
+
 			const countEl = document.getElementById('entList-count');
 			if (countEl) countEl.textContent = parseInt(data.total, 10).toLocaleString();
 
@@ -34,8 +34,8 @@ function fetchEntList(page = 1) {
 								  <tr>
 									<td>${item.cpId}</td>
 									<td><img class="entImg" src="${item.cpImgUrl}"></img></td>
-									<td>${item.memEmail}</td>
-									<td>${item.memNickname}</td>
+									<td>${item.cpName}</td>
+									<td>${item.cpRegion}</td>
 								   
 								  </tr>`).join('');
 				listEl.innerHTML = rows;
@@ -82,44 +82,75 @@ function entListPangingFn() {
 	}
 }
 
+document.getElementById("entList").addEventListener("click", function(e) {
+	const tr = e.target.closest("tr");
+	if (!tr) return;
+
+	const tds = tr.querySelectorAll("td");
+	const id = tds[0].textContent.trim();
+
+	let formData = new FormData();
+	formData.set("id", id);
+
+	entDetail(formData);
+
+});
+
+
+function entDetail(formData) {
+	axios.post('/admin/cmg/getEntDetail.do', formData)
+		.then(res => {
+			const { memberDetail, filePath, countVO, interestCn, vacByCns, counseling, avgRate } = res.data;
+			const profileImgEl = document.getElementById('member-profile-img');
+			profileImgEl.src = filePath ? filePath : '/images/defaultProfileImg.png';
+
+
+			document.getElementById('mem-id').value = memberDetail.memId || '-';
+			document.getElementById('mem-name').value = memberDetail.memName || '-';
+			document.getElementById('mem-nickname').value = memberDetail.memNickname || '-';
+			document.getElementById('mem-email').value = memberDetail.memEmail || '-';
+			document.getElementById('mem-phone').value = memberDetail.memPhoneNumber || '-';
+			document.getElementById('mem-gen').value = memberGender(memberDetail.memGen) || '-';
+			document.getElementById('mem-birth').value = formatDate(memberDetail.memBirth) || '-';
+			document.getElementById('mem-logType').value = convertLoginType(memberDetail.loginType) || '-';
+
+			const selectElement = document.getElementById("mem-role");
+
+			for (let i = 0; i < selectElement.options.length; i++) {
+				if (selectElement.options[i].value === memberDetail.memRole) {
+					selectElement.options[i].selected = true;
+					break;
+				}
+			}
+
+		})
+		.catch(error => {
+			console.error('기업 정보 불러오기 실패', error);
+		});
+}
+
+function entSearchFn() {
+
+	const searchBtn = document.getElementById('btnSearch');
+	if (searchBtn) {
+			searchBtn.addEventListener("click", function() {
+				window.currentPage = 1;
+				fetchEntList(1);
+			});
+		}
+
+}
+
+document.querySelectorAll(".tab-btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelectorAll(".tab-btn").forEach(b => b.classList.remove("active"));
+        document.querySelectorAll(".tab-content").forEach(c => c.classList.remove("active"));
+        
+        btn.classList.add("active");
+        document.getElementById(btn.dataset.tab).classList.add("active");
+    });
+});
+
+entSearchFn();
 fetchEntList();
 entListPangingFn();
-
-/*function fetchCnsList(page = 1) {
-	const pageSize = 10;
-	const keyword = document.querySelector('input[name="keyword"]').value;
-	const status = document.querySelector('select[name="status"]').value;
-
-	axios.get('/admin/umg/getMemberList.do', {
-		params: {
-			currentPage: page,
-			size: pageSize,
-			keyword: keyword,
-			status: status,
-			memRole: 'R01003'
-		}
-	})
-		.then(({ data }) => {
-			const countEl = document.getElementById('userList-count');
-			if (countEl) countEl.textContent = parseInt(data.total, 10).toLocaleString();
-
-			const listEl = document.getElementById('userList');
-			if (!listEl) return;
-
-			if (data.content.length < 1 && keyword.trim() !== '') {
-				listEl.innerHTML = `<tr><td colspan='2' style="text-align: center;">검색 결과를 찾을 수 없습니다.</td></tr>`;
-			} else {
-				const rows = data.content.map(item => `
-					  <tr>
-						<td>${item.memId}</td>
-						<td>${item.memName}</td>
-						<td>${item.memEmail}</td>
-						<td>${item.memNickname}</td>
-					   
-					  </tr>`).join('');
-				listEl.innerHTML = rows;
-			}
-			renderPagination(data);
-		})
-		.catch(err => console.error('유저 목록 조회 중 에러:', err));
-}*/
--- src/main/webapp/WEB-INF/views/admin/cmg/enterpriseManagement.jsp
+++ src/main/webapp/WEB-INF/views/admin/cmg/enterpriseManagement.jsp
@@ -1,8 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
 <link rel="stylesheet" href="css/admin/cmg/enterpriseManagement.css">
+<link rel="stylesheet"
+	href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <script src="/js/include/admin/cmg/enterpriseManagement.js"></script>
 <!-- 제목입니다 -->
-<h2 style="color: gray; font-size: 18px; margin: 0; line-height: 75px;">기업 관리</h2>
+<h2 style="color: gray; font-size: 18px; margin: 0; line-height: 75px;">기업
+	관리</h2>
 <body>
 	<div class="admin-EntMng-1" style="margin-bottom: 20px;">
 		<div class="template-panel admin-entMng-1-1">
@@ -12,9 +16,8 @@
 					<option value="1">전체</option>
 					<option value="2">기업명</option>
 					<option value="3">지역명</option>
-				</select>
-				<input type="text" name="keyword" placeholder="기업명으로 검색" />
-				<button type="button" class="btn-save">조회</button>
+				</select> <input type="text" name="keyword" placeholder="기업명으로 검색" />
+				<button type="button" class="btn-save" id="btnSearch">조회</button>
 			</div>
 			<div class="listEnt">
 				<div class="search-filter-bar">
@@ -43,12 +46,86 @@
 				</div>
 			</div>
 		</div>
-		<div class="template-panel admin-entMng-1-1">
-			<div class="middleTitle">대학 상세</div>
+
+		<div>
+			<div class="entDetail-container">
+				<div class="ent-profile-container">
+					<!-- 좌측 프로필 -->
+					<div class="ent-profile-left">
+						<div class="profile-card">
+							<img id="entLogo" src="http://www.work.go.kr/framework/filedownload/getImage.do?filePathName=tqyczLUvysXSEcOVhPgDCrrcRciqlR8qoTAjsPuEp44sKoGxe9VyqEeBMF5IUTkBaD8EvNXUzF3cvyArjHjQkV5OHVJHQRI4rj9jllpoHbs%3D" alt="기업 로고" class="profile-logo">
+							<h3 id="entName" class="profile-name">한국개발연구원국제정책대학원대학교</h3>
+							<p id="entRole" class="profile-role">South Korea / 대한민국</p>
+
+							<div class="profile-actions">
+								<div class="btn-follow">G30UNK</div>
+								<div class="btn-message">미지정</div>
+							</div>
+
+							<div class="profile-about">
+								<h4>ABOUT</h4>
+							</div>
+
+							<div class="profile-info">
+								<p>
+									<strong>기업ID:</strong>
+									<span id="entId">-</span>
+								</p>
+								<p>
+									<strong>기업명:</strong>
+									<span id="entName">contact@example.com</span>
+								</p>
+								<p>
+									<strong>기업규모:</strong>
+									<span id="entScale">-</span>
+								</p>
+								<p>
+									<strong>주소:</strong>
+									<span id="entAddress">서울특별시 강남구</span>
+								</p>
+							</div>
+
+
+						</div>
+					</div>
+
+					<!-- 우측 상세 내용 -->
+					<div>
+						<div class="ent-profile-right" style="margin-bottom: 20px;">
+							<!-- 탭 메뉴 -->
+							<div>
+								<div class="tab-menu">
+									<button class="tab-btn active" data-tab="about">About</button>
+									<button class="tab-btn" data-tab="timeline">Timeline</button>
+								</div>
+								<!-- 탭 내용 -->
+								<div class="tab-content active" id="about">
+									<div class="profile-social">
+										<a href="" target="_blank">
+											<i class="fas fa-link"></i>&nbsp;&nbsp;&nbsp; URL Link
+										</a>
+									</div>
+								</div>
+
+								<div class="tab-content" id="timeline">
+									<h4>연혁</h4>
+									<p>-</p>
+								</div>
+
+							</div>
+						</div>
+						<div class="template-panel"></div>
+					</div>
+				</div>
+			</div>
+			<div class="template-panel entAbout" id="entDetailAbout" style="margin-top: 20px; height: 330px;">
+
+			</div>
 		</div>
 	</div>
+
 	<div class="template-panel admin-entMng-2">
-		<div class="middleTitle">대학 상세 학과</div>
+		<div class="middleTitle">신규 기업 등록</div>
 	</div>
 </body>
 
