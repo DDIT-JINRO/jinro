a43131ccaf4f15f05606fefc6064d4122b256a87
5e0k1 <ksc03073@gmail.com>
2025-08-18 17:26:30 +0900

    feat : 메인페이지에서 비동기로 트렌드 컨텐츠 받아올 url매핑 추가
    기존에 컨텐츠 통계 서비스가 /admin으로 시작하는 컨트롤러의 경로로 연결되어있어서 권한불가임. 필요한 메소드 두가지만 추출해서 따로 구현.

--- src/main/java/kr/or/ddit/main/web/MainController.java
+++ src/main/java/kr/or/ddit/main/web/MainController.java
@@ -1,16 +1,40 @@
 package kr.or.ddit.main.web;
 
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import kr.or.ddit.admin.las.service.ContentStatsService;
 
 @Controller
 @RequestMapping("/")
 public class MainController {
 
+	@Autowired
+	private ContentStatsService contentStatsService;
+
 	@GetMapping
 	public String main() {
 
 		return "content/main";
+	}
+
+	@GetMapping("/bookmark/top")
+	@ResponseBody
+	public List<Map<String, Object>> bookmarkTopN(@RequestParam Map<String, Object> param){
+		return contentStatsService.bookmarkTopN(param);
+	}
+
+	@GetMapping("/community/top5/main")
+	@ResponseBody
+	public List<Map<String, Object>> communityTop5Main(@AuthenticationPrincipal String memId){
+		return contentStatsService.selectCommunityTop5PostsByMemBirth(memId);
 	}
 }
