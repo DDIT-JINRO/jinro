a06163cfbbe0433a301fbd31202799d4b912de27
5e0k1 <ksc03073@gmail.com>
2025-08-19 23:21:39 +0900

    refactor : 컨트롤러에서 작업하던 로직 서비스로 인계

--- src/main/java/kr/or/ddit/cnslt/aicns/web/AICounselingController.java
+++ src/main/java/kr/or/ddit/cnslt/aicns/web/AICounselingController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import kr.or.ddit.cnslt.aicns.service.AICounselingService;
 import kr.or.ddit.cnslt.resve.crsv.service.CounselingReserveService;
 import kr.or.ddit.mpg.pay.service.MemberSubscriptionVO;
 import kr.or.ddit.mpg.pay.service.PaymentService;
@@ -26,10 +27,7 @@ import kr.or.ddit.mpg.pay.service.PaymentVO;
 public class AICounselingController {
 
 	@Autowired
-	private CounselingReserveService counselingReserveService;
-
-	@Autowired
-	private PaymentService paymentService;
+	private AICounselingService aiCounselingService;
 
 	@GetMapping("/aicns.do")
 	public String aicns() {
@@ -39,35 +37,7 @@ public class AICounselingController {
 	@PostMapping("/aicnsPopUpStart")
 	@ResponseBody
 	public Map<String, Object> aicnsPopUpStart(HttpSession session, @RequestBody Map<String, Object> param){
-		String topic = (String) param.get("topic");
-
-		String topicKr = null;
-		switch (topic) {
-		case "MIND": {
-			topicKr = "심리상담";
-			break;
-		}
-		case "JOB": {
-			topicKr = "취업상담";
-			break;
-		}
-		case "STUDY": {
-			topicKr = "학업상담";
-			break;
-		}
-		default:
-			throw new IllegalArgumentException("Unexpected value: " + topic);
-		}
-
-		session.setAttribute("topic", topic);
-		session.setAttribute("topicKr", topicKr);
-		Map<String, Object> responseData = new HashMap<>();
-		String sessionId = session.getId();
-		responseData.put("ok", true);
-		responseData.put("message", "success");
-		responseData.put("sessionId", sessionId);
-		responseData.put("popupUrl", "/cnslt/aicns/aicnsPopUp?sid="+sessionId);
-		return responseData;
+		return aiCounselingService.aicnsPopUpStart(session, param);
 	}
 
 	@GetMapping("/aicnsPopUp")
@@ -76,25 +46,11 @@ public class AICounselingController {
 						, @AuthenticationPrincipal String memId
 						, HttpSession session
 						, HttpServletRequest req) {
-		String sessionId = session.getId();
-		// 세션아이디 비교, 회원로그인 비교, 횟수체크 .....
-		if(!sessionId.equals(sid) || memId==null || "anonymousUser".equals(memId)) {
-			model.addAttribute("errorMessage", "잘못된 접근입니다");
-		}
 
-		// 요청받은 url체크. AI상담페이지에서 버튼클릭으로 보낸 요청이 아니면 에러발생
-		String referer = req.getHeader("Referer");
-		if(referer==null || !referer.contains("/cnslt/aicns/aicns.do")) {
-			model.addAttribute("errorMessage", "비정상적인 경로입니다");
-			return "cnslt/aicns/aicnsPopUp";
-		}
-
-		// 횟수 차감. 차감실패시 에러 전송
-		MemberSubscriptionVO currentSub = paymentService.selectByMemberId(Integer.parseInt(memId));
-		PaymentVO paymentVO = counselingReserveService.selectLastSubcription(currentSub);
-		int cnt = counselingReserveService.minusPayConsultCnt(paymentVO.getPayId());
-		if(cnt <= 0) {
-			model.addAttribute("errorMessage", "상담가능 잔여횟수가 없습니다");
+		String errorMessage = aiCounselingService.validateForStartAICns(sid, memId, session, req);
+		// 정상적인 요청 확인 및 상담횟수 차감 성공시 errorMessage null값 반환됨
+		if(errorMessage!=null) {
+			model.addAttribute("errorMessage",errorMessage);
 		}
 
 		// 로그 기록
