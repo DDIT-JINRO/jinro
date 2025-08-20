2fb1604445d03387a10ad2158dcd55a7efcc61e1
KangBoram <kb6408@naver.com>
2025-08-20 20:44:21 +0900

    refactor : 또래게시판 CSS 수정

--- src/main/resources/static/js/comm/peer/teen/teenList.js
+++ src/main/resources/static/js/comm/peer/teen/teenList.js
@@ -4,15 +4,14 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 	
-	const cardList = document.querySelectorAll('.group-card');
-	cardList.forEach(card => {
-		card.addEventListener('click', function() {
-			
+	// 디테일 페이지로 이동 - userCommon.css 클래스명으로 변경
+	document.querySelectorAll('.content-list__item').forEach(item => {
+		item.addEventListener('click', function() {
 			location.href = '/comm/peer/teen/teenDetail.do?boardId=' + this.dataset.tbdId;
-		})
-	})
+		});
+	});
 
-	
+	// 글작성 버튼 이벤트
 	document.getElementById('btnWrite').addEventListener('click', function() {
 		if (!memId || memId == 'anonymousUser') {
 			sessionStorage.setItem("redirectUrl", location.href);
@@ -20,9 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
 		} else {
 			location.href = "/comm/peer/teen/teenInsert.do";
 		}
-	})
+	});
 });
-
 
 
 
