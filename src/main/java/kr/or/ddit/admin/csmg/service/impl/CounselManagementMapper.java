d11bb0447e36988d3908e4c2d2ec05ab492e6afa
NZM <nzm6846@gmail.com>
2025-08-20 11:51:54 +0900

    feat : admin 상담사 관리통계 기본구조 생성

---
+++ src/main/java/kr/or/ddit/admin/csmg/service/impl/CounselManagementMapper.java
@@ -0,0 +1,8 @@
+package kr.or.ddit.admin.csmg.service.impl;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CounselManagementMapper {
+
+}
---
+++ src/main/java/kr/or/ddit/admin/csmg/service/impl/CounselManagementServiceImpl.java
@@ -0,0 +1,10 @@
+package kr.or.ddit.admin.csmg.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import kr.or.ddit.admin.csmg.service.CounselManagementService;
+
+@Service
+public class CounselManagementServiceImpl implements CounselManagementService {
+
+}
