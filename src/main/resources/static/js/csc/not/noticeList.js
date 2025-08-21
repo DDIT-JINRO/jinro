81ac71ba8adf81279e80bf93a8ce1298502fbbe0
KangBoram <kb6408@naver.com>
2025-08-21 14:29:28 +0900

    refactor : 공지사항 리스트 CSS 수정

--- src/main/resources/static/js/csc/not/noticeList.js
+++ src/main/resources/static/js/csc/not/noticeList.js
@@ -1,16 +1,7 @@
-// 게시글 총 건수 정규식 표현(1000 단위 콤마)
-const data = document.getElementById("getAllNotice").textContent;
-const numberOnly = data.replace(/[^0-9]/g, ""); // 숫자만 추출
-const formatted = parseInt(numberOnly, 10).toLocaleString();
-document.getElementById("getAllNotice").textContent = `총 ${formatted}건`;
-
 document.addEventListener('DOMContentLoaded', function() {
-	var cards = document.querySelectorAll('.group-card');
-
-	for (var i = 0; i < cards.length; i++) {
-		cards[i].addEventListener('click', function() {
-			var noticeId = this.getAttribute('data-tbd-id');
-			window.location.href = '/csc/not/noticeDetail.do?noticeId=' + noticeId;
+	document.querySelectorAll('.content-list__item').forEach(notice => {
+		notice.addEventListener('click', () => {
+			location.href = '/csc/not/noticeDetail.do?noticeId=' + notice.dataset.noticeId;
 		});
-	}
+	});
 });
