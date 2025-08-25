de3f85ccebba6de1dab476a747b05957ae5f4452
KangBoram <kb6408@naver.com>
2025-08-25 20:48:43 +0900

    feat : 청소년 게시판 정렬 기능 추가

--- src/main/resources/static/js/comm/peer/teen/teenList.js
+++ src/main/resources/static/js/comm/peer/teen/teenList.js
@@ -20,6 +20,16 @@ document.addEventListener('DOMContentLoaded', function() {
 			location.href = "/comm/peer/teen/teenInsert.do";
 		}
 	});
+	
+	const toggleButton = document.querySelector('.search-filter__accordion-header');
+	const panel = document.querySelector('.search-filter__accordion-panel');
+
+	if (toggleButton) {
+		toggleButton.addEventListener('click', function() {
+			this.classList.toggle('is-active');
+			panel.classList.toggle('is-open');
+		});
+	}
 });
 
 
