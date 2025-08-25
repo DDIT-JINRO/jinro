4cffa6f5bbf04b5d922301a98f7dcc767352ac7a
KangBoram <kb6408@naver.com>
2025-08-25 21:36:36 +0900

    feat : 봉사활동 정렬 기능 수정

--- src/main/java/kr/or/ddit/prg/act/vol/web/ActivityVolunteerController.java
+++ src/main/java/kr/or/ddit/prg/act/vol/web/ActivityVolunteerController.java
@@ -33,7 +33,7 @@ public class ActivityVolunteerController {
 			@RequestParam(value = "contestGubunFilter", required = false) List<String> contestGubunFilter,
 			@RequestParam(value = "contestTargetFilter", required = false) List<String> contestTargetFilter,
 			@RequestParam(value = "contestStatusFilter", required = false) List<String> contestStatusFilter,
-			@RequestParam(value = "sortOrder", defaultValue = "deadline") String sortOrder) {
+			@RequestParam(value = "sortOrder", required = false) String sortOrder) {
 
 		ActivityVO activityVO = new ActivityVO();
 		activityVO.setKeyword(keyword);
